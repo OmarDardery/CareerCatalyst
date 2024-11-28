26541705eccb92ef68e85f0a4fe8af777e233517
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2024-11-28 12:31:06 +0200

    Update bingo.js

--- src/components/bingo.js
+++ src/components/bingo.js
@@ -14,7 +14,7 @@ function Bingo(props) {
                 gridTemplateColumns: "repeat(5, 1fr)",
                 gridTemplateRows: "repeat(5, 1fr)", 
                 gap: "1px",
-                height: "100%",
+                height: "50%",
                 aspectRatio: 1
             }}
         >

9f2a6dd68b92cc4a482e08f6af435d3abebc8d72
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2024-12-13 17:28:11 +0200

    added a menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,36 @@
 import React from "react";
 
-function Header(){
+function Header() {
+    const myFunction = (event) => {
+        const container = event.currentTarget;
+        container.classList.toggle("change");
+    };
+
     return (
-        <div>
-            <div style={{width: "100%", position: "fixed", display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "center", zIndex: "2", backgroundColor: "#33253e", top: "0", left: "0", right: "0", height: "10vh"}}>
-                <h1 style={{fontSize: "4vh"}}>TypologyDen</h1>
+        <div
+            style={{
+                width: "100%",
+                position: "fixed",
+                display: "flex",
+                zIndex: "2",
+                backgroundColor: "#33253e",
+                top: "0",
+                left: "0",
+                right: "0",
+                height: "10vh",
+            }}
+            className={"header"}
+        >
+            <div 
+                className="menuContainer" 
+                onClick={myFunction} 
+            >
+                <div className="menuBar1"></div>
+                <div className="menuBar2"></div>
+                <div className="menuBar3"></div>
             </div>
+            <h1 style={{ fontSize: "4vh" }}>TypologyDen</h1>
         </div>
-        
     );
 }
 

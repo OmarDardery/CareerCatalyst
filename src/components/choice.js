f22f791eed9cd7a71fe0480e1fab638914d7211e
Omar Dardery <168490867+OmarDardery@users.noreply.github.com>
2025-02-08 00:45:35 +0200

    added retrieving data and fixed some styling

--- src/components/choice.js
+++ src/components/choice.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Choice(props){
     return (
-        <div style={props.choice !== 0 ?{ display: "none"} : { display: "flex", flexDirection: "column", width: "100%", justifyContent: "space-around", alignItems: "center", gap: "15vw", paddingTop: "10vw"}}>
-            <button onClick={() => props.setChoice(1)} style={{width: "70vw"}}>
-                <span className="button_top" style={{fontSize: "8vw", padding: "5vw", borderRadius: "4vw"}}>take the test</span>
+        <div style={props.choice !== 0 ?{ display: "none"} : { display: "flex", flexDirection: "column", width: "100%", justifyContent: "space-around", alignItems: "center", gap: "15vh", paddingTop: "10vh"}}>
+            <button onClick={() => props.setChoice(1)} style={{width: "70vw", maxWidth: "400px"}}>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "clamp(16px, 5vw, 38px)", borderRadius: "clamp(16px, 4vw, 33px)"}}>take the test</span>
             </button>
-            <button onClick={() => props.setChoice(2)} style={{width: "70vw"}}>
-                <span className="button_top" style={{fontSize: "8vw", padding: "5vw", borderRadius: "4vw"}}>retrieve results</span>
+            <button onClick={() => props.setChoice(2)} style={{width: "70vw", maxWidth: "400px"}}>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "clamp(16px, 5vw, 38px)", borderRadius: "clamp(16px, 4vw, 33px)"}}>retrieve results</span>
             </button>
-            <button onClick={() => props.setChoice(3)} style={{width: "70vw"}}>
-                <span className="button_top" style={{fontSize: "8vw", padding: "5vw", borderRadius: "4vw"}}>jobs details</span>
+            <button onClick={() => props.setChoice(3)} style={{width: "70vw", maxWidth: "400px"}}>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "clamp(16px, 5vw, 38px)", borderRadius: "clamp(16px, 4vw, 33px)"}}>jobs details</span>
             </button>
         </div>
         
--- src/components/retrieveResults.js
+++ src/components/retrieveResults.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import MbtiList from "./MBTI components/mbtiNJobs";
+import cognitiveFunctions from "../cognitiveFunctionsData";
+import Chart from "./MBTI components/mbtichart";
 function Retrieve(props){
-    return (<div>
+    let [retrieved, setRetrieved] = useState(false);
+    let [data, setData] = useState([]);
+    let [id, setId] = useState('');
+    async function retrieveData(id){
+        const { data, error } = await props.supabase
+        .from('CareerCatalystData')
+        .select()
+        .eq('user_id', id);
+        if (error) {
+            console.error("Error retrieving data:", error);
+        } else {
+            console.log("Data retrieved successfully");
+            setRetrieved(true);
+            setData(data);
+        }
+        cognitiveFunctions[0].score = data[0].te;
+        cognitiveFunctions[1].score = data[0].ti;
+        cognitiveFunctions[2].score = data[0].se;
+        cognitiveFunctions[3].score = data[0].si;
+        cognitiveFunctions[4].score = data[0].ne;
+        cognitiveFunctions[5].score = data[0].ni;
+        cognitiveFunctions[6].score = data[0].fe;
+        cognitiveFunctions[7].score = data[0].fi;
+    }
 
+    return (
+    <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", width:"100%"}}>
+        <div style={ retrieved? {display: "none"} : {height: "fit-content", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", paddingLeft: "10vw", paddingRight: "10vw"}}>
+            <h1 style={{fontSize: "5vw", textAlign: "left"}}>Retrieve your data from our servers!</h1>
+            <input type="text" placeholder="Enter your user id" value={id} onChange={(event) => setId(event.target.value)} />
+            <button onClick={() => retrieveData(id)} style={{margin: "2vw", alignSelf: "flex-end"}}><span className="button_top">Retrieve</span></button>
+        </div>
+        <div style={ retrieved? {height: "fit-content", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%"} : {display: "none"}}>
+            <Chart cognitiveFunctions={cognitiveFunctions}/>
+            <MbtiList cognitiveFunctions={cognitiveFunctions}/>
+        </div>
     </div>);
 }
 

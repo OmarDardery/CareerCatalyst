1c7cc37545f4fef3db1a74bb4e0a6aca42f1a98b
Omar Dardery <168490867+OmarDardery@users.noreply.github.com>
2025-02-09 20:43:16 +0200

    changed the choice cards

--- src/components/choice.js
+++ src/components/choice.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Choice(props){
     return (
-        <div style={props.choice !== 0 ?{ display: "none"} : { display: "flex", flexDirection: "column", width: "100%", justifyContent: "space-around", alignItems: "center", gap: "11vh", paddingTop: "4vh"}}>
+        <div style={props.choice !== 0 ?{ display: "none"} : { display: "flex", flexDirection: "column", width: "100%", justifyContent: "space-around", alignItems: "center", gap: "8vh", paddingTop: "7vh"}}>
             <button onClick={() => props.setChoice(1)} style={{width: "70vw", maxWidth: "400px", borderRadius: "33px"}}>
-                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "38px clamp(16px, 5vw, 38px)", borderRadius: "33px"}}>take the test</span>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "42px clamp(16px, 6vw, 38px)", borderRadius: "33px"}}>take the test</span>
             </button>
             <button onClick={() => props.setChoice(2)} style={{width: "70vw", maxWidth: "400px", borderRadius: "33px"}}>
-                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "38px clamp(16px, 5vw, 38px)", borderRadius: "33px"}}>retrieve results</span>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "42px clamp(16px, 6vw, 38px)", borderRadius: "33px"}}>retrieve results</span>
             </button>
             <button onClick={() => props.setChoice(3)} style={{width: "70vw", maxWidth: "400px", borderRadius: "33px"}}>
-                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "38px clamp(16px, 5vw, 38px)", borderRadius: "33px"}}>jobs details</span>
+                <span className="button_top" style={{fontSize: "clamp(16px, 8vw, 38px)", padding: "42px clamp(16px, 6vw, 38px)", borderRadius: "33px"}}>jobs details</span>
             </button>
         </div>
         

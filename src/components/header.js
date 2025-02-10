01ef32f77d52bd1a08c7f581737b32fe70a5bce4
Omar Dardery <168490867+OmarDardery@users.noreply.github.com>
2025-02-10 04:03:08 +0200

    added title in the menu

--- src/components/header.js
+++ src/components/header.js
@@ -17,13 +17,20 @@ function Header(props) {
                 backgroundColor: "#33253e",
                 top: "0",
                 left: "0",
-                right: "0",
-                height: "9vh",
+                height: "10vh",
+                padding: "0px",
             }}
             className={"header"}
         >
             <div onClick={() => {menuState(false)}} style={menu ? {height: "20000%", width: "100%", backgroundColor: "black", position: "fixed", zIndex: "3", left: "0px", margin: "0px", opacity: "0.7", cursor: "pointer"} : {display: "none"}}></div>
-            <div className="menu" id = {menu? "": "closed"} style={{zIndex: "4"}}>
+            <div className="menu" id = {menu? "": "closed"} style={{zIndex: "4", color: "#21252E", padding: "0px"}}>
+                <div style={{fontSize: "2.5vw", paddingTop: "0.8vh", marginLeft: "10vh", width: "fit-content"}}>
+                    <h1 style={{fontSize: "3.5vh", margin: "5px", color: "#21252E"}}>TypologyDen:</h1>
+                    <h2 style={{fontSize: "2.5vh", margin: "5px"}}>Your career compass</h2>
+                </div>
+                <div style={{width: "100%", height: "2px", backgroundColor: "#21252E"}}>
+
+                </div>
                 <div style={{display: "flex", justifyContent: "flex-end", margin: "10px", fontSize: "1.5vh", alignSelf: "flex-end", justifySelf: "flexEnd", marginLeft: "30px"}}>
                     <label className="languageSelector" style={props.language === "english"? {} : {display: "none"}}>
                     <input onChange={props.ChangeLanguage} checked={props.language === "english"? false : true } type='checkbox' />
